2bd4b5b8b8f5b3bc505cfff56192c54eaedea0f2
GustavoSilverio <gustavosk290905@gmail.com>
2024-08-16 21:48:17 -0300

    CORS configuration

--- main.py
+++ main.py
@@ -3,11 +3,25 @@ from fastapi import FastAPI
 from pymongo import MongoClient
 from typing import List
 from models.manga import Manga
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 client = MongoClient(os.environ.get("MONGO_BASE_URL"))
 db = client["my-mangas"]
 coll = db["manga"]
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/healthy")
 def readRoot():
